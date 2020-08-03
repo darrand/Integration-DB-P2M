6cf6aeb2a0f37390472d0c5d2f7293a2f2c4a71f
darrand <darrand37@gmail.com>
2020-08-03 09:39:52 +0700

    Added pandas implementation and csv

--- filescripts/mysql_extractor.py
+++ filescripts/mysql_extractor.py
@@ -2,6 +2,7 @@ import mysql.connector as mysqldb
 import sys
 import csv
 import os
+import pandas as pd
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -13,16 +14,23 @@ def getExcel():
     DB = os.environ.get('DB')
     USER = os.environ.get('USER')
     PASS = os.environ.get('PASS')
-    TABLE = 'indonesia'
-
+    
     connect = mysqldb.connect(host=HOST, database=DB, user=USER, password=PASS)
-    query = 'SELECT * FROM ' + TABLE
     cursor = connect.cursor()
-    cursor.execute(query)
-    records = cursor.fetchall()
-    print(
-        records
-    )
+    # GETTING THE TABLES    
+    table_query = 'SHOW TABLES'
+    cursor.execute(table_query)
+    tables = [ i[0] for i in cursor.fetchall()] 
+    
+    # Records of data from mysql db
+    records = {}
+    for table in tables:
+       query = 'SELECT * FROM ' + table
+       cursor.execute(query)
+       records[table] = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
+       loc = 'csv/'+table+'.csv'
+       # Print to csv
+       records[table].to_csv(loc, index=False, header=True)
 
     if (connect.is_connected()):
         connect.close()
