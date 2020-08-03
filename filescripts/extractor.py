66f3369e3e3f1b9705f6900ea0d2f766030cb883
darrand <darrand37@gmail.com>
2020-08-03 09:39:17 +0700

    Moved package install to a function

--- filescripts/extractor.py
+++ filescripts/extractor.py
@@ -3,13 +3,16 @@ import sys
 import foxpro_extractor
 import mysql_extractor
 
-if __name__ == "__main__":
+def install_packages():
     print('installing packages...')
     packages = ["dbfread", "numpy","pandas", "python-dotenv", "xlrd", "xlwt"]
     for package in packages:
         print('installing ' + package)
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
         print(package + ' installed')
+
+if __name__ == "__main__":
+    # install_packages()
     
     # foxproData = foxpro_extractor.getExcel()
     mysqldata = mysql_extractor.getExcel()
